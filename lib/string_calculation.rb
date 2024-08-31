35416a0b22bae9339672f5e4f41f0b00d5657ecb
26Dhananjay <djaythorat1991@gmail.com>
2024-09-01 02:28:24 +0530

    Added all the steps into read me file

--- lib/string_calculation.rb
+++ lib/string_calculation.rb
@@ -1,7 +1,7 @@
 class StringCalculation
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if numbers.length == 1
+    # return numbers.to_i if numbers.length == 1
 
     delimiter = ","
 	if numbers.start_with?("//")
@@ -10,11 +10,11 @@ class StringCalculation
 
     numbers = numbers.gsub("\n", delimiter)
     num_arr = numbers.split(delimiter).map(&:to_i)
-    puts num_arr.inspect
+    # puts num_arr.inspect
     negative_nums = num_arr.select { |n| n < 0 }
     raise "negative numbers not allowed: #{negative_nums.join(',')}" unless negative_nums.empty?
 
-    puts num_arr.sum
+    # puts num_arr.sum
     num_arr.sum
 
   end
