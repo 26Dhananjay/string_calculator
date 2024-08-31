02c9485cca669078d49de2ec11ebbc2baa4901c1
26Dhananjay <djaythorat1991@gmail.com>
2024-09-01 01:20:32 +0530

    Added second case provided string contains 1 number

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe StringCalculation do
-  describe "Definition add" do
-    context "when the input provided is an empty string" do
-      it "will returns 0" do
-        expect(StringCalculation.add("")).to eq(0)
-      end
-    end
+	describe "Definition add" do
+		context "when the input provided is an empty string" do
+			it "will returns 0" do
+				expect(StringCalculation.add("")).to eq(0)
+			end
+		end
 
-   
-  end
+		context "When the provided input is single digit" do
+			it "will returns the same number" do 
+				expect(StringCalculation.add("3")).to eq(3)
+			end
+		end
+
+	end
 end
